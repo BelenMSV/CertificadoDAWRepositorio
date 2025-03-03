08706e97447d5565f9606690105a3abb90a03a36
BelenMSV <moondream19@hotmail.com>
2025-03-03 09:39:22 +0100

    default comentado ya que no es necesario

--- ejercicio11_js/3.js
+++ ejercicio11_js/3.js
@@ -38,9 +38,10 @@ switch (operacion){
       "Si a " + numA + " le restamos " + numB + " el resultado es " + resultado + "."
     );
     break;
-    default:
-      /*Puesto que el bucle do-while se repite hasta que el usuario teclea una de las cuatro opciones
-      que establecimos, nunca debería entrar en el default, a no ser que el usuario altere el valor
-      de la variable introduciendo un valor no permitido*/
-       document.write("No has seleccionado una opción válida");
+  /*   default:
+      //Puesto que el bucle do-while se repite hasta que el usuario teclea una de las cuatro opciones
+      //que establecimos, nunca debería entrar en el default, a no ser que el usuario altere el valor
+      //de la variable introduciendo un valor no permitido
+      //document.write("No has seleccionado una opción válida"); 
+       */
    }
