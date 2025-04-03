ace9ea8d030f449bb05511c85d4f73db75b49584
BelenMSV <moondream19@hotmail.com>
2025-04-03 16:23:44 +0200

    Ejercicio 17 objeto String

---
+++ cliente/cli1.2/cli1.2.1/ejercicio17_js/17.js
@@ -0,0 +1,33 @@
+/* Solicita una cadena al usuario e indica la cantidad de veces que aparece la a en las palabras impares
+Solicita una cadena al usuario y devuélvela invertida Ej: "Hola, ¿qué tal estás?" -> estás? tal ¿qué Hola, */
+
+let cadena = new String(prompt("Introduce una cadena"));
+console.log(typeof cadena);
+
+let divisiones = cadena.split(" ");
+
+let contador = 0;
+let palabraImpar;
+for (let i = 0; i < divisiones.length; i++) {
+  if (i % 2 == 0 || i == 0) {
+    palabraImpar = divisiones[i];
+    console.log(palabraImpar);
+    for (j = 0; j < palabraImpar.length; j++) {
+      if (palabraImpar[j] == "a") {
+        contador++;
+      }
+    }
+  }
+}
+alert(`La cadena de texto contiene ${contador} veces la letra a en sus palabras impares`);
+
+//Solicita una cadena al usuario y devuélvela invertida Ej: "Hola, ¿qué tal estás?"
+
+let cadena2 = new String(prompt("Introduce una cadena"));
+let divisiones2 = cadena.split(" ");
+let cadenaInvertida = new String("");
+for(let i = divisiones2.length; i>0; i--){
+   cadenaInvertida.concat(" ", divisiones2[i]);
+   console.log(cadenaInvertida);
+}
+alert(cadenaInvertida);
